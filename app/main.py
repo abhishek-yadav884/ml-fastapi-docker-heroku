bbfc71c40742ec5ad18e0dcd29f35edaa4797fa7
abhishek-yadav884 <abhishek.yadav@bristlecone.com>
2022-09-13 15:46:23 +0530

    message changed

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from app.model.model import predict_pipeline
-from app.model.model import __version__ as model_version
 
+from app.model.model import __version__ as model_version
+from app.model.model import predict_pipeline
 
 app = FastAPI()
 
@@ -17,7 +17,7 @@ class PredictionOut(BaseModel):
 
 @app.get("/")
 def home():
-    return {"health_check": "OK", "model_version": model_version}
+    return {"health_check": "OK Abhishek yadav", "model_version": model_version}
 
 
 @app.post("/predict", response_model=PredictionOut)
